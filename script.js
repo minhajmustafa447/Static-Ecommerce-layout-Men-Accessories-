3e00c5a98a50a923b3cd1d765448e9aa93793276
Minhaj_Mustafa <minhajmustafa2004@gmail.com>
2026-01-09 12:47:15 +0500

    Update Data

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function feedbackMsg(){
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    let Msg = document.getElementById("Msg").value
+
+    console.log(username);
+    console.log(email);
+    console.log(phone);
+    console.log(Msg);
+
+    alert("Your Feedback Submitted successfully")
+}
